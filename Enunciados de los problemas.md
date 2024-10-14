5bf21afc42033c69a8424519a8a4392e5331be33
MatteoConte00 <matteo2.conte@mail.polimi.it>
2024-10-14 18:40:49 +0200

    Description problemas y resolucion

---
+++ Enunciados de los problemas.md
@@ -0,0 +1,34 @@
+---
+editor_options: 
+  markdown: 
+    wrap: sentence
+---
+### **Problema 1: Tabla**
+
+La tabla de analizar es la siguiente:
+
+   d1 d2 d3 d4 
+e1  3 25  2 18
+e2 23 35 28  9
+e3 39  7  7 32
+
+### **Problema 2: Estrategia para un Plan de Expansión Empresarial**
+
+Una cadena de restaurantes está planificando la expansión en tres nuevas ciudades.
+Las opciones son:
+
+-   **Ciudad A**: Una gran metrópoli con alta demanda pero también con alta competencia.
+-   **Ciudad B**: Una ciudad de tamaño mediano con menor competencia.
+-   **Ciudad C**: Una ciudad pequeña con bajos costos operativos, pero con una demanda potencialmente reducida.
+
+El éxito de la expansión depende de cómo los consumidores respondan a la apertura del restaurante, lo que puede ser: - **Alta demanda**: Muchos consumidores y alta rentabilidad.
+- **Demanda media**: Demanda estable y beneficios moderados.
+- **Baja demanda**: Baja afluencia de clientes y beneficios limitados.
+
+### **Tabla de Decisión (Beneficios estimados en miles de euros)**
+
+| Ciudad       | Alta demanda (k€) | Demanda media (k€) | Baja demanda (k€) |
+|--------------|-------------------|--------------------|-------------------|
+| **Ciudad A** | 600               | 400                | 100               |
+| **Ciudad B** | 900               | 600                | 200               |
+| **Ciudad C** | 1200              | 700                | 50                |
---
+++ Enunciados y resolucion.R
@@ -0,0 +1,78 @@
+#Problema1 (solo tabla)----
+
+La tabla de analizar es la siguiente:
+  
+  d1 d2 d3 d4 
+e1  3 25  2 18
+e2 23 35 28  9
+e3 39  7  7 32
+
+#Wald
+criterio.Wald(X)
+criterio.Wald(X,favorable=F)
+
+#Optimista
+criterio.Optimista(X)
+criterio.Optimista(X,favorable = F)
+
+#Hurwicz
+dibuja.criterio.Hurwicz(X)
+dibuja.criterio.Hurwicz_Intervalos(X)
+dibuja.criterio.Hurwicz(X,favorable = F)
+dibuja.criterio.Hurwicz_Intervalos(X,favorable = F)
+
+#Savage
+criterio.Savage(X)
+criterio.Savage(X,favorable = F)
+
+#Laplace
+criterio.Laplace(X)
+criterio.Laplace(X,favorable = F)
+
+#Punto_Ideal
+criterio.PuntoIdeal(X)
+criterio.PuntoIdeal(X,favorable = F)
+
+#Problema2 (testo y solucion)
+
+Una cadena de restaurantes está planificando la expansión en tres nuevas ciudades.
+Las opciones son:
+  
+  -   **Ciudad A**: Una gran metrópoli con alta demanda pero también con alta competencia.
+-   **Ciudad B**: Una ciudad de tamaño mediano con menor competencia.
+-   **Ciudad C**: Una ciudad pequeña con bajos costos operativos, pero con una demanda potencialmente reducida.
+
+El éxito de la expansión depende de cómo los consumidores respondan a la apertura del restaurante, lo que puede ser: - **Alta demanda**: Muchos consumidores y alta rentabilidad.
+- **Demanda media**: Demanda estable y beneficios moderados.
+- **Baja demanda**: Baja afluencia de clientes y beneficios limitados.
+
+### **Tabla de Decisión (Beneficios estimados en miles de euros)**
+
+| Ciudad       | Alta demanda (k€) | Demanda media (k€) | Baja demanda (k€) |
+  |--------------|-------------------|--------------------|-------------------|
+  | **Ciudad A** | 600               | 400                | 100               |
+  | **Ciudad B** | 900               | 600                | 200               |
+  | **Ciudad C** | 1200              | 700                | 50                |
+  
+
+vec2<-c(600,400,100,900,600,200,1200,700,50)#valores en k€
+X2<-crea.tablaX(vec2,3,3)
+
+#Wald
+criterio.Wald(X2)
+
+#Optimista
+criterio.Optimista(X2)
+
+#Hurwicz
+dibuja.criterio.Hurwicz(X2)
+dibuja.criterio.Hurwicz_Intervalos(X2)
+
+#Savage
+criterio.Savage(X2)
+
+#Laplace
+criterio.Laplace(X2)
+
+#Punto_Ideal
+criterio.PuntoIdeal(X2)
